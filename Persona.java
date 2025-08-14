cb197796ca1543b80b222dbc49c04d5b3fc9c6fa
Edgar <edhuman@outlook.com>
2025-08-14 12:49:39 -0500

    priemr commit

---
+++ Persona.java
@@ -0,0 +1,9 @@
+public class Persona{
+	public static void main(String [] args){
+	if(args.length<1){
+		System.out.println("Hola mundo..!");
+	}else{
+		System.out.println("Hola"+args[0]+"..!");
+	}
+}
+}
